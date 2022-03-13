7871cdaecbfa9e784d5b7debbf574107e3a9442d
Daniel <morohoschidanieliosif@gmail.com>
2022-03-14 00:45:26 +0200

    modified code to change date

--- web-app-licenta/server.js
+++ web-app-licenta/server.js
@@ -2,7 +2,7 @@ const path = require( 'path' );
 const express = require( 'express' );
 const socket = require( 'socket.io' );
 
-const { moistureDatabase, smokeDatabase, temperatureAndHumidityDatabase } = require( './firebase/admin' );
+const { pathDatabase, moistureDatabase, smokeDatabase, temperatureAndHumidityDatabase } = require( './firebase/admin' );
 
 // import LED control API
 const { toggle } = require( './led-api' );
@@ -43,8 +43,37 @@ app.post('/motion-sensor-data', (req, res) => {
 //---------GET MOTION SENSOR DATA--------
 app.post('/get-temperature-data', (req, res) => {
   let sht21Data = req.body;
-  let currentTime = require('./methods/getTime');
-  console.log(currentTime);
+
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+
+  if(seconds < 10){
+      seconds ='0' + seconds;
+  }
+  if(minutes < 10){
+      minutes ='0' + minutes;
+  }
+  if(hours < 10){
+      hours ='0' + hours;
+  }
+  
+  const currentTime = hours + ':' + minutes;
+  const currentDate = date + '-' + month + '-' + year;
+
+  const previousData = pathDatabase.child(`sensors/temperature-and-humidity/${currentDate}/${currentTime}`);
+
+  if(minutes == '30' || minutes == '00'){
+    previousData.set({
+      temperature: sht21Data.temperature,
+      humidity : sht21Data.humidity
+    })
+  }
   console.log(sht21Data);
   io.emit('sht21Event',sht21Data);
   res.send("Data received");
@@ -53,6 +82,37 @@ app.post('/get-temperature-data', (req, res) => {
 //---------GET SMOKE SENSOR DATA--------
 app.post('/get-smoke-data', (req, res) => {
   let mq135Data = req.body;
+  
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+
+  if(seconds < 10){
+      seconds ='0' + seconds;
+  }
+  if(minutes < 10){
+      minutes ='0' + minutes;
+  }
+  if(hours < 10){
+      hours ='0' + hours;
+  }
+  
+  const currentTime = hours + ':' + minutes;
+  const currentDate = date + '-' + month + '-' + year;
+
+  const previousData = pathDatabase.child(`sensors/smoke/${currentDate}/${currentTime}`);
+
+  if(minutes == '30' || minutes == '00'){
+    previousData.set({
+      CO: mq135Data.C0,
+      CO2 : mq135Data.Smoke
+    })
+  }
   console.log(mq135Data);
   io.emit('mq135Event',mq135Data);
   res.send("Data received");
