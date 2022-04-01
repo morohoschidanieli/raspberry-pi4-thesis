bfaef14a067b254aad7b47a360a32996f59f1650
Morohoschi Daniel <morohoschidanieliosif@gmail.com>
2022-04-01 15:13:52 +0300

    added camera and arrow for control of camera and source

--- web-app-licenta/api-routes/home.js
+++ web-app-licenta/api-routes/home.js
@@ -2,7 +2,7 @@
 const path = require( 'path' );
 
 module.exports = function(app){
-app.get( '/overview', ( request, response ) => {
+app.get( '/', ( request, response ) => {
     response.sendFile( path.resolve( __dirname, '../public/html/index.html' ), {
       headers: {
         'Content-Type': 'text/html',
--- web-app-licenta/public/ui-src/js/camera.js
+++ web-app-licenta/public/ui-src/js/camera.js
@@ -2,6 +2,7 @@ var cameraModule = {
 
     $toggleRecordingButton: $("#toggle-recording-action"),
     $toggleCaptureButton: $("#toggle-capture-action"),
+    $toggleStreamButton: $("#toggle-stream"),
 
     $startingRecordingMessage: $(".js-recording-text"),
 
@@ -10,9 +11,21 @@ var cameraModule = {
             $(".js-recording-text").toggleClass('hidden');
             $(this).toggleClass('active');
         })
+    },
+
+    toggleVideoStream: function(){
+        this.$toggleStreamButton.click(function(){
+            $(".fa-video-slash").toggleClass("stop-camera start-camera");
+            if($(".fa-video-slash").hasClass('start-camera')){
+                $("#stream-src").src="stream.mjpg";
+            }else{
+                $("#stream-src").src="/assets/img/video-stream/no-video-stream.png";
+            }
+        })
     }
 
 };
 
 // Outputs: Where in the world is Paul Irish today?
 cameraModule.toggleCapture();
+cameraModule.toggleVideoStream();
