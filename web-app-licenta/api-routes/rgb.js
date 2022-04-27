206d95f450028c7f5333a4558d74feb06550f165
Daniel <morohoschidanieliosif@gmail.com>
2022-04-28 01:29:43 +0300

    added color picker

--- web-app-licenta/api-routes/rgb.js
+++ web-app-licenta/api-routes/rgb.js
@@ -3,15 +3,13 @@ const { pathDatabase } = require( '../secrets/firebase/admin.js' );
 
 module.exports = function(app, io){
     app.post('/rgb-data', (req, res) => {
-        let rgbData = (req.body.favcolor);
-        const r = parseInt(rgbData.substr(1,2), 16);
-        const g = parseInt(rgbData.substr(3,2), 16);
-        const b = parseInt(rgbData.substr(5,2), 16);
+        let rgbData = (req.body);
+
         const previousData = pathDatabase.child(`sensors/rgb-data`);
         previousData.set({
-          red: r,
-          green : g,
-          blue :  b
+          red: rgbData.red,
+          green : rgbData.green,
+          blue :  rgbData.blue
         })
   
         //io.emit('motionSensorEvent',{"isActivated":motionSensorData.isActivated});
